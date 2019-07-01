0d285fd6f8d285df61a4b63aa3326d2195db43a0
Bianca <bianca.rocha1711@gmail.com>
2019-07-01 09:21:31 -0300

    final tests and finally seeing the application

---
+++ tests/unit/api.spec.js
@@ -0,0 +1,20 @@
+import flushPromises from 'flush-promises';
+import nock from 'nock';
+import api from '@/api';
+import userFixture from './fixtures/user';
+
+describe('api', () => {
+  it('searches for the user', async () => {
+    const expectedUser = 'kuroski';
+
+    const request = nock('https://api.github.com')
+      .get(`/users/${expectedUser}`)
+      .reply(200, userFixture);
+
+    const result = await api.searchUser(expectedUser);
+    await flushPromises();
+
+    expect(result).toEqual(userFixture);
+    expect(request.isDone()).toBe(true);
+  });
+});
